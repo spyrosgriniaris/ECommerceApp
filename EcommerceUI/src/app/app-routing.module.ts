b1e7b32ae0f70e4d895e1c1496b48762f66cad12
spyrosgriniaris <spyros.gr21@gmail.com>
2021-01-09 13:49:18 +0200

    Styling the website

--- EcommerceUI/src/app/app-routing.module.ts
+++ EcommerceUI/src/app/app-routing.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
-import { ShopComponent } from './shop/shop.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'test-error', component: TestErrorComponent},
-  {path: 'server-error', component: ServerErrorComponent},
-  {path: 'not-found', component: NotFoundComponent},
+  {path: '', component: HomeComponent, data: {breadcrump: 'Home'}},
+  {path: 'test-error', component: TestErrorComponent, data: {breadcrump: 'Test Errors'}},
+  {path: 'server-error', component: ServerErrorComponent, data: {breadcrump: 'Server Error'}},
+  {path: 'not-found', component: NotFoundComponent, data: {breadcrump: 'Not Found'}},
   // shop module will be activated when we access the shop path and not from the beggining
-  {path: 'shop', loadChildren: () => import('././shop/shop.module').then(mod => mod.ShopModule)},
+  {path: 'shop', loadChildren: () => import('././shop/shop.module').then(mod => mod.ShopModule), data: {breadcrump: 'Shop'}},
   // {path: 'shop/:id', component: ProductDetailsComponent},
-  {path: '**', redirectTo: '', pathMatch: 'full'}
+  {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
 @NgModule({
--- EcommerceUI/src/app/app.module.ts
+++ EcommerceUI/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
+import { NgxSpinnerModule } from 'ngx-spinner'
 
 
 import { AppComponent } from './app.component';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
 
 
 @NgModule({
@@ -21,10 +23,12 @@ import { ErrorInterceptor } from './core/interceptors/error.interceptor';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    HomeModule
+    HomeModule,
+    NgxSpinnerModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
